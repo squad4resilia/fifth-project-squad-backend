d88fc7372689fd62c356dc53f41f7ad161783682
Ysmael Marques <ysmaelmarkss@gmail.com>
2023-02-14 15:40:24 -0300

    tst

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import app from "./src/app.js";
+import  cors from "cors";
 
 let port = process.env.PORT || 3000;
+
+app.use(cors({
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }));
 
 app.listen(port, () => {
     console.log(`server working ${port}`)
